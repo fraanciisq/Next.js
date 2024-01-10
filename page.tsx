95bc2c27c911a49e752c89b7b267153a482c5b49
Francis Tamondong <88927642+fraanciisq@users.noreply.github.com>
2024-01-10 23:26:01 +0800

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+ <main className='h-full w-full'>
+  <div className='flex flex-col gap-20'>
+
+  </div>
+    Francis Portfolio
+  </main>
+  )
+}
