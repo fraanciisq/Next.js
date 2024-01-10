de5a333519f024cc9da4102ff9b3ae9868ea0c70
Francis Tamondong <88927642+fraanciisq@users.noreply.github.com>
2024-01-11 00:04:26 +0800

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
+import Hero from '@/components/main/Hero';
+import Image from 'next/image';
 
 export default function Home() {
   return (
- <main className='h-full w-full'>
-  <div className='flex flex-col gap-20'>
-
+   <main className="h-full w-full">
+  <div className="flex flex-col h-[850px] gap-20">
+    <Hero />
   </div>
-    Francis Portfolio
   </main>
-  )
+  );
 }
